f3e243453fb594947410d67940012599cad1e61b
Joel Kattapuram <joelkattapuram@berkeley.edu>
2019-03-30 16:44:55 -0700

    joel test

--- public/app.js
+++ public/app.js
@@ -9,3 +9,23 @@ async function GoogleLogin() {
   document.querySelector("#userIntro").innerHTML =
     "Welcome, " + user.displayName;
 }
+
+function uploadFile(files) {
+  const storageRef = firebase.storage().ref();
+  const imgRef = storageRef.child('trash.png');
+
+  const file = files.item(0);
+
+  const task = imgRef.put(file)
+
+  // successful upload
+  task.then(snapshot => {
+      const url = snapshot.downloadURL
+  })
+
+  // monitor progress
+  task.on('state_changed', snapshot => {
+      console.log(snapshot)
+
+  })
+}
