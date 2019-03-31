706bc8e7db21a49ceb33f445db89dbac5f3e62f4
Rithwik Ediga Lakhamsani <ricky.ediga@gmail.com>
2019-03-30 17:10:26 -0700

    added goal reading

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", event => {
   const app = firebase.app();
+  getGoals("jpKiOJ9QYxblUvHM3G2Y");
 });
 
 async function GoogleLogin() {
@@ -12,20 +13,34 @@ async function GoogleLogin() {
 
 function uploadFile(files) {
   const storageRef = firebase.storage().ref();
-  const imgRef = storageRef.child('trash.png');
+  const imgRef = storageRef.child("trash.png");
 
   const file = files.item(0);
 
-  const task = imgRef.put(file)
+  const task = imgRef.put(file);
 
   // successful upload
   task.then(snapshot => {
-      const url = snapshot.downloadURL
-  })
+    const url = snapshot.downloadURL;
+  });
 
   // monitor progress
-  task.on('state_changed', snapshot => {
-      console.log(snapshot)
+  task.on("state_changed", snapshot => {
+    console.log(snapshot);
+  });
+}
 
-  })
+async function getGoals(name) {
+  const goals = firebase.firestore().collection("goals");
+  const post = goals.doc(name);
+  const doc = await post.get();
+  const data = doc.data();
+  console.log(data);
+  document.querySelector("#goal-holder").innerHTML =
+    "Name: " +
+    data.name +
+    " End-Date: (we need to convert this mofo)" +
+    data.end.seconds +
+    " Status: " +
+    data.status;
 }
