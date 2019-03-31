3c86cd4cbb153d4bc824ffeaf41a46794e21c7b3
Abhi Arya <abhiarya@berkeley.edu>
2019-03-30 21:20:03 -0700

    add goal

--- public/app.js
+++ public/app.js
@@ -30,4 +30,28 @@ function uploadFile(files) {
   })
 }
 
-function updateGoal
+function checkForm() {
+  var f = document.forms["goal"].elements;
+  var cansubmit = true;
+  for (var i = 0; i < f.length; i++) {
+    if (f[i].value.length == 0) cansubmit = false;
+  }
+  document.getElementById('goal').disabled = !cansubmit;
+}
+
+function addGoal() {
+  const db = firebase.firestore();
+  const goals = db.collection("goals");
+  goals.add({
+    name: document.querySelector("#name").value,
+    description: document.querySelector("#description").value,
+    start: firebase.firestore.Timestamp.fromDate(new Date(document.querySelector("#start").value)),
+    end: firebase.firestore.Timestamp.fromDate(new Date(document.querySelector("#end").value))
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
+}
